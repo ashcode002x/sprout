6060bb8db38a5ba256085a2af86379e860b67d60
Aashish <aashish836863@gmail.com>
2025-04-17 12:11:18 +0530

    csrf token updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
 
 
 # Application definition
